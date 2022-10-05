0937766d58cd3b7b692959af8e675a1f84231193
Matin Sadeghi <matinsadegi3290@gmail.com>
2022-10-05 13:53:49 +0330

    prepare hederes and add token

--- client/src/api/apiSlice.js
+++ client/src/api/apiSlice.js
@@ -2,26 +2,13 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
-  tagTypes:['Form'],
-  endpoints: (builder) => ({
-    signUp: builder.mutation({
-      query: (formData) => ({
-        url:'/user/signup',
-        method:'POST',
-        body:formData
-      }),
-      invalidatesTags:['Form']
-    }),
-    signIn: builder.mutation({
-      query: (formData) => ({
-        url:'/user/signin',
-        method:'POST',
-        body:formData
-      }),
-      invalidatesTags:['Form']
-    }),
-  }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000', prepareHeaders:(headers) => {
+    if(JSON.parse(localStorage.getItem('profile'))){
+      const { token } = JSON.parse(localStorage.getItem('profile'));
+      headers.set("authorization", `Bearer ${token}`)
+    }
+    return headers
+  } }),
+  tagTypes: ['Form', 'User'],
+  endpoints: builder => ({})
 });
-
-export const { useSignUpMutation, useSignInMutation } = apiSlice;
