c350df8be23989dbab6f4c6990dd7d4970c32749
matin <matinsadegi3290@gmail.com>
2022-11-01 09:59:41 +0330

    install cloudinary and setup

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,28 +1,35 @@
-import bcrypt from 'bcryptjs';
-import { validationResult } from 'express-validator';
-import User from '../models/user.js';
-import generateToken from '../utils/generateToken.js';
+import bcrypt from "bcryptjs";
+import { validationResult } from "express-validator";
+import User from "../models/user.js";
+import generateToken from "../utils/generateToken.js";
+import cloudinary from "../utils/cloudinary.js";
 
 //SignUp
 export const signUp = async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-
+  const { firstName, lastName, email, password, avatar } = req.body;
   const error = validationResult(req);
   if (!error.isEmpty()) {
     console.log(error.array());
-    // return res.status(422).json({ errors: error.array()[0]});
+    return res.status(422).json({ errors: error.array()[0] });
   }
   try {
+    const result = await cloudinary.uploader.upload(avatar , {
+      folder: "profilePic"
+    })
     const hashedPassword = await bcrypt.hash(password, 12);
     const user = await User.create({
       firstName,
       lastName,
       email,
+      avatar: {
+        public_id: result.public_id,
+        url: result.secure_url,
+      },
       password: hashedPassword,
     });
     res.status(200).json({ user, token: generateToken(user._id) });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong...' });
+    res.status(500).json({ message: "Something went wrong..." });
   }
 };
 //SignIn
@@ -37,20 +44,19 @@ export const signIn = async (req, res) => {
     .status(200)
     .json({ existingUser, token: generateToken(existingUser._id) });
 };
- 
+
 //GET All users
 export const getUsers = async (req, res) => {
   const keyword = req.query.search
     ? {
         $or: [
-          { firstName: { $regex: req.query.search, $options: 'i' } },
-          { lastName: { $regex: req.query.search, $options: 'i' } },
-          { email: { $regex: req.query.search, $options: 'i' } },
+          { firstName: { $regex: req.query.search, $options: "i" } },
+          { lastName: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
         ],
       }
     : {};
-   
+
   const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
   res.json(users);
-};  
- 
+};
