18c5403fb302e4625e1ce78d7e469e62d02a354f
matin <matinsadegi3290@gmail.com>
2022-11-01 10:02:11 +0330

    convert image file to base64

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useSignUpMutation } from '../../features/auth/authApiSlice';
-import { setProfile } from '../../features/auth/authSlice.js';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useSignUpMutation } from "../../features/auth/authApiSlice";
+import { setProfile } from "../../features/auth/authSlice.js";
 
 const initialState = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  password: '',
-  confirmPassword: '',
+  firstName: "",
+  lastName: "",
+  avatar: {},
+  email: "",
+  password: "",
+  confirmPassword: "",
 };
 
 const Signup = ({ setErr }) => {
@@ -24,56 +25,74 @@ const Signup = ({ setErr }) => {
     e.preventDefault();
     try {
       const result = await signUp(form).unwrap();
+      console.log(result);
       dispatch(setProfile(result));
-      navigate('/');
-    } catch (error) {
-      const { errors } = error.data;
-      setErr(errors);
+      navigate("/");
+    } catch (err) {
+      // const { errors } = error.data;
+      // setErr(errors);
+      console.log(err);
     }
   };
+//handle and convert it in base64
+  const handleImage = (e) => {
+    const pics = e.target.files[0];
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FileReader();
+      data.readAsDataURL(pics);
+      data.onloadend = () => {
+        setForm({ ...form, avatar: data.result });
+      };
+      
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} noValidate>
       <div>
         <input
-          name='firstName'
-          type='text'
-          placeholder='First Name'
+          name="firstName"
+          type="text"
+          placeholder="First Name"
           onChange={handleChange}
           required
         />
         <input
-          name='lastName'
-          type='text'
-          placeholder='Last Name'
+          name="lastName"
+          type="text"
+          placeholder="Last Name"
           onChange={handleChange}
           required
         />
       </div>
       <input
-        name='email'
-        type='email'
-        placeholder='Email'
+        name="email"
+        type="email"
+        placeholder="Email"
         onChange={handleChange}
         required
       />
       <input
-        name='password'
-        type='password'
-        placeholder='Password'
+        name="password"
+        type="password"
+        placeholder="Password"
         onChange={handleChange}
         required
       />
       <input
-        name='confirmPassword'
-        type='password'
-        placeholder='Confirm Password'
+        name="confirmPassword"
+        type="password"
+        placeholder="Confirm Password"
         onChange={handleChange}
         required
       />
-
+      <label className="custom-file" onChange={handleImage}>
+        <input name="avatar" type="file" />
+        Upload Profile Image
+      </label>
       <button
-        type='submit'
-        className='auth__form-container_fields-content_button'
+        type="submit"
+        className="auth__form-container_fields-content_button"
       >
         Sign Up
       </button>
