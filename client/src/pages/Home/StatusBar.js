ec5c8aee5585d69ec4244ca013826e636110d48c
Matin Sadegi <73706293+MatinSadegi@users.noreply.github.com>
2022-11-20 09:34:16 +0330

    Update StatusBar.js

--- client/src/pages/Home/StatusBar.js
+++ client/src/pages/Home/StatusBar.js
@@ -14,12 +14,7 @@ const StatusBar = () => {
   return (
     <div className="status-bar__container">
       <div className="status-bar__top">
-        <img
-          src={`${userInfo.avatar.url}`}
-          className="profile-img"
-          onClick={() => setShowItems(!showItems)}
-          alt="avatar"
-        />
+       
 
         <motion.div
           className="items"
