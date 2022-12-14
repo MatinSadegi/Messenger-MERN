7576d5cf7dc80f74bafead34ff558b49bcb2533f
Matin Sadegi <73706293+MatinSadegi@users.noreply.github.com>
2022-12-14 10:27:35 +0330

    update base url

--- client/src/services/apiSlice.js
+++ client/src/services/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000', prepareHeaders:(headers) => {
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://messenger-egep.onrender.com', prepareHeaders:(headers) => {
     if(JSON.parse(localStorage.getItem('profile'))){
       const { token } = JSON.parse(localStorage.getItem('profile'));
       headers.set("authorization", `Bearer ${token}`)
