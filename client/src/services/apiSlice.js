95e9617a8ef3de065df79b052dff393910b6937e
matin <matinsadegi3290@gmail.com>
2023-07-01 21:17:42 +0330

    change baseUrl

--- client/src/services/apiSlice.js
+++ client/src/services/apiSlice.js
@@ -1,14 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000', prepareHeaders:(headers) => {
-    if(JSON.parse(localStorage.getItem('profile'))){
-      const { token } = JSON.parse(localStorage.getItem('profile'));
-      headers.set("authorization", `Bearer ${token}`)
-    }
-    return headers
-  } }),
-  tagTypes: ['Form', 'User', 'Message'],
-  endpoints: builder => ({})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://messenger-egep.onrender.com",
+    prepareHeaders: (headers) => {
+      if (JSON.parse(localStorage.getItem("profile"))) {
+        const { token } = JSON.parse(localStorage.getItem("profile"));
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Form", "User", "Message"],
+  endpoints: (builder) => ({}),
 });
